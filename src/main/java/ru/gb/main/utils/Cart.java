974b59732fdf1ac26f52f02c05e45265b4a6d5e1
Алексей <t1meal@yandex.ru>
2022-05-24 14:42:28 +0300

    hm2 final

--- src/main/java/ru/gb/main/utils/Cart.java
+++ src/main/java/ru/gb/main/utils/Cart.java
@@ -1,4 +1,43 @@
 package ru.gb.main.utils;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class Cart {
+
+    private final ProductRepository productRepository;
+    private List<Product> cartProductList;
+
+    @Autowired
+    public Cart(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+    @PostConstruct
+    private void setCartProductList (){
+        this.cartProductList = new ArrayList<>();
+    }
+
+    public void addProductForIndex (int i){
+       cartProductList.add(productRepository.getProductForIndex(i));
+    }
+    public void removeProductForIndex(int i){
+        for (Product element: cartProductList) {
+            if (element.getId() == i){
+                cartProductList.remove(element);
+                return;
+            }
+        }
+    }
+    public List<Product> getCartProductList() {
+        return cartProductList;
+    }
+
 }
