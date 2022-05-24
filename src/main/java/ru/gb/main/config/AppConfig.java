55059dd6dc5d3332e0c9510a3813468694cdb990
Алексей <t1meal@yandex.ru>
2022-05-24 14:38:10 +0300

    hm2 final

--- src/main/java/ru/gb/main/config/AppConfig.java
+++ src/main/java/ru/gb/main/config/AppConfig.java
@@ -1,9 +1,4 @@
 package ru.gb.main.config;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan ("ru.gb.main")
 
 public class AppConfig {
 }
--- src/main/java/ru/gb/main/utils/ProductRepository.java
+++ src/main/java/ru/gb/main/utils/ProductRepository.java
@@ -1,35 +1,5 @@
 package ru.gb.main.utils;
 
-import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-@Component
 public class ProductRepository {
-    private List<Product> productList;
 
-    @PostConstruct
-    private void primarySet() {
-        productList = new ArrayList<>(Arrays.asList(
-                new Product(1, "bread"),
-                new Product(2, "milk"),
-                new Product(3, "cheese"),
-                new Product(4, "meat"),
-                new Product(5, "chicken")));
-    }
-
-    public Product getProductForIndex(int i) {
-        for (Product element: productList) {
-            if (element.getId() == i){
-                return element;
-            }
-        }
-        return null;
-    }
-    public List<Product> getAllProducts() {
-        return productList;
-    }
 }
